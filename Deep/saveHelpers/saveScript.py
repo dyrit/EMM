422341cbd72afc97eaaee9a1abd0c2b6ef82905e
dyrit <dy2507@rit.edu>
2024-02-09 03:29:39 -0500

    first commit

--- Deep/saveHelpers/saveScript.py
+++
@@ -1,9 +0,0 @@
-import csv
-import os
-
-def save_losses(save_name, header = None, values=['0','0']):
-    f = open(save_name, 'a')
-    writer = csv.writer(f)
-    if header is not None:
-        writer.writerow(header)
-    writer.writerow(values)
